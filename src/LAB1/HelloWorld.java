86a729b0df97e0c24cf7012b83c1ec2ffa68f7a6
suphaset <sup202582@rmutsvmail.com>
2020-12-08 11:05:27 +0700

    Initial commit

---
+++ src/LAB1/HelloWorld.java
@@ -0,0 +1,23 @@
+package LAB1;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+       System.out.println("HelloWorld");
+
+
+
+
+
+    }//main
+
+
+
+
+
+
+
+
+
+
+
+}//class
