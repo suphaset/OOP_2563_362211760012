89463486eb653a61fc0af588147235cda79d8936
suphaset <sup202582@rmutsvmail.com>
2020-12-29 11:51:30 +0700

    1

---
+++ src/LAB2/BasicInputData.java
@@ -0,0 +1,28 @@
+package LAB2;
+
+import java.util.Scanner;
+
+public class BasicInputData {
+
+        public  static  void  main (String[] args){
+            Scanner scanner = new Scanner(System.in);
+
+
+
+            System.out.println("Enter an integer: ");
+        int num = scanner.nextInt();
+
+        System.out.println("The number is "+num);
+
+
+
+
+
+
+    }//main
+
+
+
+
+
+}//class
---
+++ src/LAB2/Exercise_STD_Profile.java
@@ -0,0 +1,40 @@
+package LAB2;
+
+import java.util.Scanner;
+
+public class Exercise_STD_Profile {
+
+    public  static  void  main (String[] args){
+
+        Scanner scanner =new Scanner(System.in);
+        System.out.print("Enter name: ");
+        String Name =scanner.nextLine();
+
+        System.out.print("Enter Std ID: ");
+        String Id =scanner.nextLine();
+
+        System.out.print("Enter age: ");
+        int age = Integer.parseInt(scanner.nextLine());
+
+        System.out.print("Enter tel: ");
+        String tel =scanner.nextLine();
+
+        System.out.print("Enter Email: ");
+        String Email =scanner.nextLine();
+1
+
+        System.out.println(Name);
+        System.out.println(Id);
+        System.out.println(age);
+        System.out.println(tel);
+        System.out.println(Email);
+
+
+
+
+
+
+    }//main
+
+
+}//class
