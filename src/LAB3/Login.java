949ad1ff84b676debb01546c9746e1d981eba231
NITRO 5 <sup202582@rmutsvmail.com>
2021-01-06 13:19:52 +0700

    4

---
+++ src/LAB3/Login.java
@@ -0,0 +1,34 @@
+package LAB3;
+
+import java.util.Scanner;
+
+
+public class Login {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int countWrong = 0;
+
+        while (countWrong<3){
+            System.out.println("Username : ");
+            String usr = scanner.nextLine();
+
+            System.out.println("password : ");
+            String pwd = scanner.nextLine();
+            if (usr.equals("admin") && pwd.equals("saiyai"))
+            {
+                System.out.println("Welcome to MT Website.");
+                break;
+            }
+            else {
+                System.out.println("username or password not correct.");
+                countWrong++;
+            }
+            if (countWrong==3)
+            {
+                System.out.println("You account has been locked. Please, contect admin.");
+            }
+        }
+    }//main
+}//class
+
