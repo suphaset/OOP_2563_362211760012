631e898dae1ee3242edfbfde8a76370486939828
NITRO 5 <sup202582@rmutsvmail.com>
2021-01-21 06:05:42 +0700

    6

---
+++ src/LAB5/Exercise_LAB5_2.java
@@ -0,0 +1,35 @@
+package LAB5;
+
+import java.util.Scanner;
+
+public class Exercise_LAB5_2 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Integers : ");
+        int num = sc.nextInt();
+
+        findValue(num);
+
+}
+
+    private static void findValue(int num) {
+        int count = 0;
+        for (int i = num; i >= 0 ; i--) {
+            if (i%3==0 && i%5==0) {
+              System.out.println(i + " " );
+              count++;
+
+            }
+            if (count == 3) {
+                break;
+            }
+        }
+
+
+
+
+
+    }
+}
