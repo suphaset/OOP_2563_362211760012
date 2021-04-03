e868874c297dc9589934a1e60bc8e7a2188e8eac
suphaset <75650629+suphaset@users.noreply.github.com>
2021-04-03 22:57:35 +0700

    9

--- src/L1/L11.java
+++ src/L1/L11.java
@@ -4,8 +4,6 @@ public class L11 {
     public static void main(String[] args) {
         System.out.println("HelloWorld");
         System.out.println("HelloWorld");
-        System.out.println("HelloWorld");
-
 
 
 
